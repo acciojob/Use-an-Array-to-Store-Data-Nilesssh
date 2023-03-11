2085c4c61377f94471e916a21ad436febcec1fd6
Nilesssh <103926034+Nilesssh@users.noreply.github.com>
2023-03-11 13:33:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 
-const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango", true , 45 , false, undefined, "nice"];
 const citrus = fruits.splice(1);
 
 document.getElementById("demo").innerHTML = citrus;
