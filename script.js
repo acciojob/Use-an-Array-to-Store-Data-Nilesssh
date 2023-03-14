ca15953f8e271bc341c6ea6c3ae7734194821a90
Nilesssh <103926034+Nilesssh@users.noreply.github.com>
2023-03-14 22:50:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
 
-const nilesh = ["nilesh", null , undefined, 123 , true , false , 1.2 ];
+let nilesh = ["nilesh", null , undefined, 123 , true , false , 1.2 ];
 
 document.getElementsByName("li").innerHTML = nilesh.length ;
