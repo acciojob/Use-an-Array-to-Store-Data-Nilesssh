fbe06d657c89685c6ee20dc6736045d6014576d0
Nilesssh <103926034+Nilesssh@users.noreply.github.com>
2023-04-21 11:24:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-let yourArray = [];
-
-document.getElementById("nice").innerHTML = yourArray ;
+let str = "";
+let nice = [1,"hello", true , "nice", false];
+nice.forEach(element => {
+	str+=`<li>${element}</li>`;
+})
+document.getElementsByTagName("ol")[0].innerHTML = str ;
