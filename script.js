b9510f8c88fc5fc36722a53a81f5dea1114e24c1
Nilesssh <103926034+Nilesssh@users.noreply.github.com>
2023-05-11 15:18:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,2 @@
-let str = "";
-let nice = [1,"hello", true , "nice", false];
-nice.forEach(element => {
-	str+=`<li>${element}</li>`;
-})
-let abc = `<ol>${str}</ol>`
-document.getElementsByTagName("body")[0].innerHTML = abc ;
+let yourArray = ['apple', 123, true, null, 'banana'];
+return yourArray;
